1c7389e0966f1d17bcda082220c6c6a84c14742a
Victor Snickers <vad.ba2014@gmail.com>
2025-06-15 15:28:59 +0300

    BLL: Added sevice interfaces

---
+++ BLL/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using BLL.DTOs.Auth;
+using BLL.DTOs.User;
+
+namespace BLL.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> RegisterAsync(UserRegisterDto registerDto);
+    Task<AuthResponseDto> LoginAsync(UserLoginDto loginDto);
+    Task<AuthResponseDto> RefreshTokenAsync(RefreshTokenDto refreshTokenDto);
+    Task RevokeTokenAsync(string refreshToken);
+}
---
+++ BLL/Interfaces/IGoalService.cs
@@ -0,0 +1,10 @@
+﻿using BLL.DTOs.Goal;
+
+namespace BLL.Interfaces;
+
+public interface IGoalService
+{
+    Task<GoalResponseDto> SetGoalAsync(Guid userId, GoalSetDto goalDto);
+    Task<GoalResponseDto> GetCurrentGoalAsync(Guid userId);
+    Task DeactivateGoalAsync(Guid goalId, Guid userId);
+}
